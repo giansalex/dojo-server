0e6a946abbb258f9b8d2f3ffb3a0e4f3559688ed
Giancarlos Salas <giansalex@gmail.com>
2019-06-14 13:08:50 -0500

    initial commit

---
+++ App.API/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace App.API.Models
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+        [BsonElement]
+        public string CategoryName { get; set; }
+        [BsonElement]
+        public string Description { get; set; }
+    }
+}
