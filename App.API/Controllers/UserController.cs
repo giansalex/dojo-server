ec397237609aa6e81b40c30b413506619f5dd7e4
eiq21 <inca.quispe.e@outlook.com>
2019-06-14 14:06:38 -0500

    creación de la clase user

---
+++ App.API/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace App.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private IMongoCollection<User> collection;
+        public UserController()
+        {
+            var client = new MongoClient("mongodb://admin:admin@10.0.75.1:27017");
+            IMongoDatabase db = client.GetDatabase("Demo01");
+            this.collection = db.GetCollection<User>("User");
+        }
+
+        public IActionResult Index()
+        {
+            var model = collection.Find(FilterDefinition<User>.Empty).ToList();
+            return Ok(model);
+        }
+        [HttpPost]
+        public IActionResult RegisterUser(User user)
+        {
+            var model = collection.Find(a => a.Nombres == user.Nombres);
+            if (model != null)
+            {
+                return BadRequest();
+            }
+            collection.InsertOne(user);
+            var usuarioInsertado = collection.Find(a => a.Nombres == user.Nombres);
+            return Ok(usuarioInsertado);
+
+        }
+    }
+}
---
+++ App.API/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.API.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string UserId { get; set; }
+        [BsonElement]
+        public string Email { get; set; }
+        [BsonElement]
+        public string Password { get; set; }
+        [BsonElement]
+        public string Apellidos { get; set; }
+        [BsonElement]
+        public string Nombres { get; set; }
+    }
+}
